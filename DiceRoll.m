b852b61bdda5658db990a99cf062b732596ee69a
Jennifer Lindgren <jennifer.lindgren93@gmail.com>
2017-05-03 16:03:51 +0200

    Woops...

---
+++ DiceRoll.m
@@ -0,0 +1,6 @@
+function result = DiceRoll(numDice)
+%% Rolls numDice dice and returns the resulting values.
+
+result = randi([1 6],1,numDice);    % Randomize numDice integers between 1 and 6.
+
+end

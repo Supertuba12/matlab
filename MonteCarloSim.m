67f7e6eaae593a919814075784d4fd24ab2ffd24
Hampus Eriksson <hamer848@student.liu.se>
2017-05-08 09:15:53 +0200

    Started adding 2.8 to montecarlosim

--- MonteCarloSim.m
+++ MonteCarloSim.m
@@ -14,4 +14,23 @@ function MonteCarloSim(iterations)
     expectedValue = sum(result)/iterations;
     variance = (1/(iterations - 1)) * (sum((result - expectedValue).^2));
     fprintf('Expected value: %d\nVariance: %d\n', expectedValue, variance);
+    
+    
+    % --- 2.8 ---
+    hold on;
+    % Sum om bars in histogram is equal to the amount of carried out
+    % experiments 
+    % Sum of probability is always 1. Multiply prob. with iterations.
+    A = [0, 1/6, 1/36, 1/216, 1/1296;
+         0, 5/6, 10/36, 15/216, 25/1296;
+         0, 0, 25/36, 80/216, 250/1296;
+         0, 0, 0, 120/216, 900/1296;
+         0, 0, 0, 0, 120/1296];
+    e1 = eye(5,1);
+    e5 = [0;0;0;0;1];
+    probability = zeros(1, max(result));
+    for k = 1:max(result)
+        probability(k) = ((e1')*(A^k)*e5)*(iterations/10);
+    end
+    histogram(probability, 'Facecolor', 'r'); 
 end

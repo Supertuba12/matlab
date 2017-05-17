76390260a47b83ac2ffe7d52cf4f1844d74502a5
Jennifer Lindgren <jennifer.lindgren93@gmail.com>
2017-05-17 07:40:20 +0200

    minor changes

--- DistributionHistogram.m
+++ DistributionHistogram.m
@@ -2,9 +2,9 @@ function DistributionHistogram(numDice)
 %% Rolls numDice dice and plots a histogram with the resulting eye numbers.
 rollData = randi([1 6],1,numDice);  % Rolls 1000 dice (1000x1-matrix).
 close all;  % Close all open figures.
-histogram(rollData,'FaceColor','magenta');    % Creates a histogram of dice rolls.
+histogram(rollData,'FaceColor','blue');    % Creates a histogram of dice rolls.
 title(sprintf('Roll distribution of %d rolls.', numDice));
-xlabel('Eye Number Shown');
+xlabel('Number of eyes shown');
 ylabel('Occurances');
 set(gca,'Xtick',1:6);
 end
